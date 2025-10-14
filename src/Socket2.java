17671aa8746a43030fefa4e966a0ed6d3831519e
jumplyon <joaopedrofariasdasilva444@gmail.com>
2025-10-13 22:05:27 -0300

    initial commit

---
+++ src/Socket2.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+
+public class Socket2 {
+    public static void main( String[] args ) {
+        try{
+            Socket sock = new Socket("www.google.com.br", 80);
+
+        }
+    }
+}
