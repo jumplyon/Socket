c43ee2735a2859f43371834c3ffc95e5c6c09e2f
jumplyon <joaopedrofariasdasilva444@gmail.com>
2025-10-16 23:13:25 -0300

    começando a adicionar a conexão com servidor

--- src/Socket2.java
+++ src/Socket2.java
@@ -3,11 +3,16 @@ import java.io.*;
 
 public class Socket2 {
     public static void main( String[] args ) {
-        try{
+        try {
             Socket sock = new Socket("www.google.com.br", 80);
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String linha="";
+             out.println("GET / HTTP/1.0\n");
+             while ((linha = in.readLine()) != null) {
 
 
-            catch
+
         }
 
     }
